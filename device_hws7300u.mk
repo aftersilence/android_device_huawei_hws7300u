892ebeb0e7f8ff714ac1d58aaf7421db51e651c9
aftersilence <913704@mail.ru>
2012-09-28 16:30:26 +0400

    Reorganization

--- device_hws7300u.mk
+++ device_hws7300u.mk
@@ -46,28 +46,28 @@ DEVICE_PACKAGE_OVERLAYS += device/huawei/hws7300u/overlay
 
 # Modules
 PRODUCT_COPY_FILES += \
-    device/huawei/hws7300u/prebuilt/modules/qce.ko:system/lib/modules/qce.ko \
-    device/huawei/hws7300u/prebuilt/modules/sch_dsmark.ko:system/lib/modules/sch_dsmark.ko \
-    device/huawei/hws7300u/prebuilt/modules/spidev.ko:system/lib/modules/spidev.ko \
-    device/huawei/hws7300u/prebuilt/modules/ksapi.ko:system/lib/modules/ksapi.ko \
-    device/huawei/hws7300u/prebuilt/modules/rpc_server_handset.ko:system/lib/modules/rpc_server_handset.ko \
-    device/huawei/hws7300u/prebuilt/modules/qcrypto.ko:system/lib/modules/qcrypto.ko \
-    device/huawei/hws7300u/prebuilt/modules/dal_remotetest.ko:system/lib/modules/dal_remotetest.ko \
-    device/huawei/hws7300u/prebuilt/modules/tsif_chrdev.ko:system/lib/modules/tsif_chrdev.ko \
-    device/huawei/hws7300u/prebuilt/modules/lcd.ko:system/lib/modules/lcd.ko \
-    device/huawei/hws7300u/prebuilt/modules/tun.ko:system/lib/modules/tun.ko \
-    device/huawei/hws7300u/prebuilt/modules/ansi_cprng.ko:system/lib/modules/ansi_cprng.ko \
-    device/huawei/hws7300u/prebuilt/modules/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
-    device/huawei/hws7300u/prebuilt/modules/librasdioif.ko:system/lib/modules/librasdioif.ko \
-    device/huawei/hws7300u/prebuilt/modules/mt9m114.ko:system/lib/modules/mt9m114.ko \
-    device/huawei/hws7300u/prebuilt/modules/cls_flow.ko:system/lib/modules/cls_flow.ko \
-    device/huawei/hws7300u/prebuilt/modules/dma_test.ko:system/lib/modules/dma_test.ko \
-    device/huawei/hws7300u/prebuilt/modules/evbug.ko:system/lib/modules/evbug.ko \
-    device/huawei/hws7300u/prebuilt/modules/msm_tsif.ko:system/lib/modules/msm_tsif.ko \
-    device/huawei/hws7300u/prebuilt/modules/dhd.ko:system/lib/modules/dhd.ko \
-    device/huawei/hws7300u/prebuilt/modules/gspca_main.ko:system/lib/modules/gspca_main.ko \
-    device/huawei/hws7300u/prebuilt/modules/msm-buspm-dev.ko:system/lib/modules/msm-buspm-dev.ko \
-    device/huawei/hws7300u/prebuilt/modules/qcedev.ko:system/lib/modules/qcedev.ko
+    device/huawei/hws7300u/prebuilt/lib/modules/qce.ko:system/lib/modules/qce.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/sch_dsmark.ko:system/lib/modules/sch_dsmark.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/spidev.ko:system/lib/modules/spidev.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/ksapi.ko:system/lib/modules/ksapi.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/rpc_server_handset.ko:system/lib/modules/rpc_server_handset.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/qcrypto.ko:system/lib/modules/qcrypto.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/dal_remotetest.ko:system/lib/modules/dal_remotetest.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/tsif_chrdev.ko:system/lib/modules/tsif_chrdev.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/lcd.ko:system/lib/modules/lcd.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/tun.ko:system/lib/modules/tun.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/ansi_cprng.ko:system/lib/modules/ansi_cprng.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/librasdioif.ko:system/lib/modules/librasdioif.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/mt9m114.ko:system/lib/modules/mt9m114.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/cls_flow.ko:system/lib/modules/cls_flow.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/dma_test.ko:system/lib/modules/dma_test.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/evbug.ko:system/lib/modules/evbug.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/msm_tsif.ko:system/lib/modules/msm_tsif.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/dhd.ko:system/lib/modules/dhd.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/gspca_main.ko:system/lib/modules/gspca_main.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/msm-buspm-dev.ko:system/lib/modules/msm-buspm-dev.ko \
+    device/huawei/hws7300u/prebuilt/lib/modules/qcedev.ko:system/lib/modules/qcedev.ko
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -178,35 +178,69 @@ PRODUCT_COPY_FILES += \
 
 # Scripts
 PRODUCT_COPY_FILES += \
-    device/huawei/hws7300u/prebuilt/scripts/init.qcom.fm.sh:system/etc/init.qcom.fm.sh \
-    device/huawei/hws7300u/prebuilt/scripts/init.qcom.modem_links.sh:system/etc/init.qcom.modem_links.sh \
-    device/huawei/hws7300u/prebuilt/scripts/init.qcom.mdm_links.sh:system/etc/init.qcom.mdm_links.sh \
-    device/huawei/hws7300u/prebuilt/scripts/init.qcom.coex.sh:system/etc/init.qcom.coex.sh \
-    device/huawei/hws7300u/prebuilt/scripts/init.hw.insmod.sh:system/etc/init.hw.insmod.sh \
-    device/huawei/hws7300u/prebuilt/scripts/init.brcm.bt.sh:system/etc/init.brcm.bt.sh \
-    device/huawei/hws7300u/prebuilt/scripts/test.bt.sh:system/etc/test.bt.sh \
-    device/huawei/hws7300u/prebuilt/scripts/init.bt.sh:system/etc/init.bt.sh \
-    device/huawei/hws7300u/prebuilt/scripts/init.qcom.sdio.sh:system/etc/init.qcom.sdio.sh \
-    device/huawei/hws7300u/prebuilt/scripts/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
-    device/huawei/hws7300u/prebuilt/scripts/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh \
-    device/huawei/hws7300u/prebuilt/scripts/init.qcom.wifi.sh:system/etc/init.qcom.wifi.sh \
-    device/huawei/hws7300u/prebuilt/scripts/bluetooth_power.sh:system/etc/bluetooth_power.sh
+    device/huawei/hws7300u/prebuilt/etc/scripts/init.qcom.fm.sh:system/etc/init.qcom.fm.sh \
+    device/huawei/hws7300u/prebuilt/etc/scripts/init.qcom.modem_links.sh:system/etc/init.qcom.modem_links.sh \
+    device/huawei/hws7300u/prebuilt/etc/scripts/init.qcom.mdm_links.sh:system/etc/init.qcom.mdm_links.sh \
+    device/huawei/hws7300u/prebuilt/etc/scripts/init.qcom.coex.sh:system/etc/init.qcom.coex.sh \
+    device/huawei/hws7300u/prebuilt/etc/scripts/init.hw.insmod.sh:system/etc/init.hw.insmod.sh \
+    device/huawei/hws7300u/prebuilt/etc/scripts/init.brcm.bt.sh:system/etc/init.brcm.bt.sh \
+    device/huawei/hws7300u/prebuilt/etc/scripts/test.bt.sh:system/etc/test.bt.sh \
+    device/huawei/hws7300u/prebuilt/etc/scripts/init.bt.sh:system/etc/init.bt.sh \
+    device/huawei/hws7300u/prebuilt/etc/scripts/init.qcom.sdio.sh:system/etc/init.qcom.sdio.sh \
+    device/huawei/hws7300u/prebuilt/etc/scripts/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
+    device/huawei/hws7300u/prebuilt/etc/scripts/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh \
+    device/huawei/hws7300u/prebuilt/etc/scripts/init.qcom.wifi.sh:system/etc/init.qcom.wifi.sh \
+    device/huawei/hws7300u/prebuilt/etc/scripts/bluetooth_power.sh:system/etc/bluetooth_power.sh
+
+# Firmware
+PRODUCT_COPY_FILES += \
+    device/huawei/hws7300u/prebuilt/etc/firmware/cyttsp_8660_ffa.hex:system/etc/firmware/cyttsp_8660_ffa.hex \
+    device/huawei/hws7300u/prebuilt/etc/firmware/cyttsp_8660_fluid_p2.hex:system/etc/firmware/cyttsp_8660_fluid_p2.hex \
+    device/huawei/hws7300u/prebuilt/etc/firmware/cyttsp_8660_fluid_p3.hex:system/etc/firmware/cyttsp_8660_fluid_p3.hex \
+    device/huawei/hws7300u/prebuilt/etc/firmware/dsps_fluid.b00:system/etc/firmware/dsps_fluid.b00 \
+    device/huawei/hws7300u/prebuilt/etc/firmware/dsps_fluid.b01:system/etc/firmware/dsps_fluid.b01 \
+    device/huawei/hws7300u/prebuilt/etc/firmware/dsps_fluid.b02:system/etc/firmware/dsps_fluid.b02 \
+    device/huawei/hws7300u/prebuilt/etc/firmware/dsps_fluid.b03:system/etc/firmware/dsps_fluid.b03 \
+    device/huawei/hws7300u/prebuilt/etc/firmware/dsps_fluid.mdt:system/etc/firmware/dsps_fluid.mdt \
+    device/huawei/hws7300u/prebuilt/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
+    device/huawei/hws7300u/prebuilt/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
+    device/huawei/hws7300u/prebuilt/etc/firmware/vidc_1080p.fw:system/etc/firmware/vidc_1080p.fw \
+    device/huawei/hws7300u/prebuilt/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+    device/huawei/hws7300u/prebuilt/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
+    device/huawei/hws7300u/prebuilt/etc/firmware/t1320_tm1828_001.img:system/etc/firmware/t1320_tm1828_001.img \
+    device/huawei/hws7300u/prebuilt/etc/firmware/t1320_tm1885_004.img:system/etc/firmware/t1320_tm1885_004.img \
+    device/huawei/hws7300u/prebuilt/etc/firmware/s7020_suc.img:system/etc/firmware/s7020_suc.img \
+    device/huawei/hws7300u/prebuilt/etc/firmware/atmel_cmi_pro.img:system/etc/firmware/atmel_cmi_pro.img
+
+# Adreno libs
+PRODUCT_COPY_FILES += \
+    device/huawei/hws7300u/prebuilt/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
+    device/huawei/hws7300u/prebuilt/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
+    device/huawei/hws7300u/prebuilt/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+    device/huawei/hws7300u/prebuilt/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+    device/huawei/hws7300u/prebuilt/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
+    device/huawei/hws7300u/prebuilt/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
+    device/huawei/hws7300u/prebuilt/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
+    device/huawei/hws7300u/prebuilt/lib/libC2D2.so:system/lib/libC2D2.so \
+    device/huawei/hws7300u/prebuilt/lib/libgsl.so:system/lib/libgsl.so \
+    device/huawei/hws7300u/prebuilt/lib/libOpenVG.so:system/lib/libOpenVG.so \
+    device/huawei/hws7300u/prebuilt/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
 
 # Some misc configuration files
 PRODUCT_COPY_FILES += \
-    device/huawei/hws7300u/prebuilt/vold.fstab:system/etc/vold.fstab \
-    device/huawei/hws7300u/prebuilt/t1320.idc:system/usr/idc/t1320.idc
+    device/huawei/hws7300u/prebuilt/etc/vold.fstab:system/etc/vold.fstab \
+    device/huawei/hws7300u/prebuilt/usr/idc/t1320.idc:system/usr/idc/t1320.idc
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
-    device/huawei/hws7300u/prebuilt/keylayout/8660_handset.kl:system/usr/keylayout/8660_handset.kl \
-    device/huawei/hws7300u/prebuilt/keylayout/fluid-keypad.kl:system/usr/keylayout/fluid-keypad.kl \
-    device/huawei/hws7300u/prebuilt/keylayout/ffa-keypad.kl:system/usr/keylayout/ffa-keypad.kl
+    device/huawei/hws7300u/prebuilt/usr/keylayout/8660_handset.kl:system/usr/keylayout/8660_handset.kl \
+    device/huawei/hws7300u/prebuilt/usr/keylayout/fluid-keypad.kl:system/usr/keylayout/fluid-keypad.kl \
+    device/huawei/hws7300u/prebuilt/usr/keylayout/ffa-keypad.kl:system/usr/keylayout/ffa-keypad.kl
 
 # Custom media config
 PRODUCT_COPY_FILES += \
-    device/huawei/hws7300u/prebuilt/media_profiles.xml:system/etc/media_profiles.xml \
-    device/huawei/hws7300u/prebuilt/media_codecs.xml:system/etc/media_codecs.xml
+    device/huawei/hws7300u/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml \
+    device/huawei/hws7300u/prebuilt/etc/media_codecs.xml:system/etc/media_codecs.xml
 
 ## misc
 PRODUCT_PROPERTY_OVERRIDES += \
