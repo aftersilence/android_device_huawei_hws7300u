5ba005509ec5ddb007173ce9e65cbe4ead63cb58
aftersilence <913704@mail.ru>
2012-10-06 00:31:53 +0400

    Some updates

--- prebuilt/etc/init.qcom.post_boot.sh
+++ prebuilt/etc/init.qcom.post_boot.sh
@@ -91,6 +91,8 @@ case "$target" in
 	 echo 5 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential 
 	 echo 192000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 	 echo 192000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+	 echo 1188000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+	 echo 1188000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
 	 chown system /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
 	 chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 	 chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
