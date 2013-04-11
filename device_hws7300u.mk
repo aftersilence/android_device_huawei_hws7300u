3f9fa60d6c3e64066e304fe06f8f7cfe7a44408a
aftersilence <yuryginpp@gmail.com>
2013-04-11 16:15:31 +0600

    Cleanup

--- device_hws7300u.mk
+++ device_hws7300u.mk
@@ -209,8 +209,7 @@ PRODUCT_COPY_FILES += \
     device/huawei/hws7300u/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml \
     device/huawei/hws7300u/prebuilt/etc/media_codecs.xml:system/etc/media_codecs.xml \
     device/huawei/hws7300u/prebuilt/etc/audio_effects.conf:system/etc/audio_effects.conf \
-    device/huawei/hws7300u/prebuilt/etc/audio_policy.conf:system/etc/audio_policy.conf \
-    device/huawei/hws7300u/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+    device/huawei/hws7300u/prebuilt/etc/audio_policy.conf:system/etc/audio_policy.conf
 
 # Auto loading of gamepad modules	
 PRODUCT_COPY_FILES += \
